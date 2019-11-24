824586591b3d29601acf5494c160974ecdb0b448
Qianheorgan <2238571572@qq.com>
2019-11-24 20:16:55 +0800

    二叉树镜像

---
+++ ReverseTree.java
@@ -0,0 +1,18 @@
+ public  class ReverseTree{
+
+     /*public void Mirror(TreeNode root) {
+
+         TreeNode tmp = null;
+         if (root != null)
+         {
+             tmp = root.left;
+             root.left = root.right;
+             root.right = tmp;
+             if (root.left != null)
+                 Mirror(root.left);
+             if (root.right != null)
+                 Mirror(root.right);
+         }
+     }*/
+ }
+
