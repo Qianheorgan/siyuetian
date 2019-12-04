fd0ad3dc7927d08cce254bc650941f58be1edb13
Qianheorgan <2238571572@qq.com>
2019-12-04 23:11:07 +0800

    Xu4_20.java

---
+++ Xu4_20.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Xu4_20 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("请输入字符串:");
+        String xu=input.nextLine();
+        System.out.println(xu.length());
+        System.out.println(xu.charAt(0));
+    }
+
+}
