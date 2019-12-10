5c18dbd68e29b5999179f4b8e2038bab6c34b5d2
Qianheorgan <2238571572@qq.com>
2019-12-10 23:44:53 +0800

    反转链表

---
+++ Reverse.java
@@ -0,0 +1,30 @@
+/*
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}*/
+public class Reverse {
+    public ListNode ReverseList(ListNode head) {
+
+        ListNode pre = null;
+        //初始化p指针，用于记录当前结点的下一个结点地址
+        ListNode p = null;
+        //head指向null时，循环终止。
+        while(head != null){
+            //先用p指针记录当前结点的下一个结点地址。
+            p = head.next;
+            //让被当前结点与链表断开并指向前一个结点pre。
+            head.next = pre;
+            //pre指针指向当前结点
+            pre = head;
+            //head指向p(保存着原链表中head的下一个结点地址)
+            head = p;
+        }
+        return pre;//当循环结束
+
+    }
+}
