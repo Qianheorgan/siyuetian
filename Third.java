f2054e6f99927aa6777192b02136040bf36c0e32
Qianheorgan <2238571572@qq.com>
2020-01-06 21:25:17 +0800

    距离

---
+++ Third.java
@@ -0,0 +1,112 @@
+public class Third{
+    public static void main(String[] args) {
+        double a1=getDistance(116.41667, 39.91667,121.43333, 34.5);
+        System.out.println("北京到上海距离:"+a1);
+        double a2=getDistance(116.41667, 39.91667,113.2333, 23.16667);
+        System.out.println("北京到广州距离:"+a2);
+        double a3=getDistance(116.41667, 39.91667,114.06667, 22.61667);
+        System.out.println("北京到深圳距离:"+a3);
+        double a4=getDistance(116.41667, 39.91667,103.7333, 36.03333);
+        System.out.println("北京到兰州距离:"+a4);
+        double a5=getDistance(116.41667, 39.91667,113, 28.21667);
+        System.out.println("北京到长沙距离:"+a5);
+        double a6=getDistance(116.41667, 39.91667,118.78113, 32.05);
+        System.out.println("北京到南京距离:"+a6);
+        double a7=getDistance(116.41667, 39.91667,104.06667, 30.6667);
+        System.out.println("北京到成都距离:"+a7);
+        double a8=getDistance(116.41667, 39.91667,108.45, 34.26667);
+        System.out.println("北京到西安距离:"+a8);
+        double a9=getDistance(116.41667, 39.91667,112.53, 37.86667);
+        System.out.println("北京到太原距离:"+a9);
+        double b1=getDistance(121.43333, 34.5,113.2333, 23.16667);
+        System.out.println("上海到广州距离:"+b1);
+        double b2=getDistance(121.43333, 34.5,114.06667, 22.61667);
+        System.out.println("上海到深圳距离:"+b2);
+        double b3=getDistance(121.43333, 34.5,103.7333, 36.03333);
+        System.out.println("上海到兰州距离:"+b3);
+        double b4=getDistance(121.43333, 34.5,113, 28.21667);
+        System.out.println("上海到长沙距离:"+b4);
+        double b5=getDistance(121.43333, 34.5,118.78113, 32.05);
+        System.out.println("上海到南京距离:"+b5);
+        double b6=getDistance(121.43333, 34.5,104.06667, 30.6667);
+        System.out.println("上海到成都距离:"+b6);
+        double b7=getDistance(121.43333, 34.5,108.45, 34.26667);
+        System.out.println("上海到西安距离:"+b7);
+        double b8=getDistance(121.43333, 34.5,112.53, 37.86667);
+        System.out.println("上海到太原距离:"+b8);
+        double c1=getDistance(113.2333, 23.16667,114.06667, 22.61667);
+        System.out.println("广州到深圳距离:"+c1);
+        double c2=getDistance(113.2333, 23.16667,103.7333, 36.03333);
+        System.out.println("广州到兰州距离:"+c2);
+        double c3=getDistance(113.2333, 23.16667,113, 28.21667);
+        System.out.println("广州到长沙距离:"+c3);
+        double c4=getDistance(113.2333, 23.16667,118.78113, 32.05);
+        System.out.println("广州到南京距离:"+c4);
+        double c5=getDistance(113.2333, 23.16667,104.06667, 30.6667);
+        System.out.println("广州到成都距离:"+c5);
+        double c6=getDistance(113.2333, 23.16667,108.45, 34.26667);
+        System.out.println("广州到西安距离:"+c6);
+        double c7=getDistance(113.2333, 23.16667,112.53, 37.86667);
+        System.out.println("广州到太原距离:"+c7);
+        double d1=getDistance(113.2333, 23.16667,103.7333, 36.03333);
+        System.out.println("深圳到兰州距离:"+d1);
+        double d2=getDistance(113.2333, 23.16667,113, 28.21667);
+        System.out.println("深圳到长沙距离:"+d2);
+        double d3=getDistance(113.2333, 23.16667,118.78113, 32.05);
+        System.out.println("深圳到南京距离:"+d3);
+        double d4=getDistance(113.2333, 23.16667,104.06667, 30.6667);
+        System.out.println("深圳到成都距离:"+d4);
+        double d5=getDistance(113.2333, 23.16667,108.45, 34.26667);
+        System.out.println("深圳到西安距离:"+d5);
+        double d6=getDistance(113.2333, 23.16667,112.53, 37.86667);
+        System.out.println("深圳到太原距离:"+d6);
+        double e1=getDistance(103.7333, 36.03333,113, 28.21667);
+        System.out.println("兰州到长沙距离:"+e1);
+        double e2=getDistance(103.7333, 36.03333,118.78113, 32.05);
+        System.out.println("兰州到南京距离:"+e2);
+        double e3=getDistance(103.7333, 36.03333,104.06667, 30.6667);
+        System.out.println("兰州到成都距离:"+e3);
+        double e4=getDistance(103.7333, 36.03333,108.45, 34.26667);
+        System.out.println("兰州到西安距离:"+e4);
+        double e5=getDistance(103.7333, 36.03333,112.53, 37.86667);
+        System.out.println("兰州到太原距离:"+e5);
+        double f1=getDistance(113, 28.21667,118.78113, 32.05);
+        System.out.println("长沙到南京距离:"+f1);
+        double f2=getDistance(113, 28.21667,104.06667, 30.6667);
+        System.out.println("长沙到成都距离:"+f2);
+        double f3=getDistance(113, 28.21667,108.45, 34.26667);
+        System.out.println("长沙到西安距离:"+f3);
+        double f4=getDistance(113, 28.21667,112.53, 37.86667);
+        System.out.println("长沙到太原距离:"+f4);
+        double g1=getDistance(118.78113, 32.05,104.06667, 30.6667);
+        System.out.println("南京到成都距离:"+g1);
+        double g2=getDistance(118.78113, 32.05,108.45, 34.26667);
+        System.out.println("南京到西安距离:"+g2);
+        double g3=getDistance(118.78113, 32.05,112.53, 37.86667);
+        System.out.println("南京到太原距离:"+g3);
+        double h1=getDistance(104.06667, 30.6667,108.45, 34.26667);
+        System.out.println("成都到西安距离:"+h1);
+        double h2=getDistance(104.06667, 30.6667,112.53, 37.86667);
+        System.out.println("成都到太原距离:"+h2);
+        double i1=getDistance(108.45, 34.26667,112.53, 37.86667);
+        System.out.println("西安到太原距离:"+i1);
+        double []arr1={a1,a2,a3,a4,a5,a6,a7,a8,a9,b1,b2,b3,b4,b5,b6,b7,b8,c1,c2,c3,c4,c5,c6,c7,d1,d2,d3,d4,d5,d6,e1,
+                e2,e3,e4,e5,f1,f2,f3,f4,g1,g2,g3,h1,h2,i1};
+        System.out.println("最小距离:"+min(arr1));
+    }
+    public static double getDistance(double lat1 ,double lng1,double lat2,double lng2) {
+        double a = lat1 - lat2;
+        double b = lng1 - lng2;
+        double s = Math.sqrt(a*a*90+b*b*111);
+        return s;
+    }
+    public static double min(double [] arr){
+        double min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+}
