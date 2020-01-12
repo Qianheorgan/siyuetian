2b2e6b43ee85c15d6e7258b100b9c3091dbb0746
Qianheorgan <2238571572@qq.com>
2020-01-12 22:20:43 +0800

    字符流中第一个不重复的字符

---
+++ NotFirstString.java
@@ -0,0 +1,21 @@
+public class NotFirstString {
+
+    int[] count = new int[256]; // 字符出现的次数
+      int[] index = new int[256]; // 字符出现的次数
+      int number = 0;
+      public void Insert(char ch) {
+          count[ch]++;
+          index[ch] = number++;
+      }
+      public char FirstAppearingOnce() {
+          int minIndex = number;
+          char ch = '#';
+          for (int i = 0; i < 256; i++) {// !!!
+                if (count[i] == 1 && index[i] < minIndex) {
+                    ch = (char) i;
+                    minIndex = index[i];
+                }
+          }
+          return ch;
+      }
+}
