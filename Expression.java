ac09107269a378be14692bf856a2b5950e53ff55
Qianheorgan <2238571572@qq.com>
2020-01-20 22:58:23 +0800

    表达式求值

---
+++ Expression.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Expression{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int[] d = new int[4];
+        d[0] = a + b + c;
+        d[1] = (a + b) * c;
+        d[2] = a + (b + c);
+        d[3] = a * b * c;
+        Arrays.sort(d);
+        System.out.println(d[3]);
+    }
+}
