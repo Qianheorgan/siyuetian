9fd76549fcb1dc97e6c48ae1366f61867fefe55b
Qianheorgan <2238571572@qq.com>
2020-01-22 22:50:39 +0800

    T11.java

---
+++ T11.java
@@ -0,0 +1,11 @@
+import com.sun.corba.se.impl.orbutil.CacheTable;
+
+public class T11 {
+    public static void main(String[] args) {
+        int x=20;
+        int y=5;
+        System.out.println(x+y+""+(x+y)+y);
+        int a=1234;
+        System.out.println(a%10);
+    }
+}
