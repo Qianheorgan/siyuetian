f03fe6f28d086ee219c098a115a62c1129264668
Qianheorgan <2238571572@qq.com>
2020-01-26 22:41:01 +0800

    中心索引

---
+++ T73.java
@@ -0,0 +1,19 @@
+public class T73 {
+
+    public static int pivotIndex(int[] nums) {
+        int sumTotal = 0;
+        int sumLeft = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sumTotal += nums[i];
+        }
+        for (int p = 0; p < nums.length; p++) {
+
+            if (sumLeft * 2 == sumTotal - nums[p]) {
+                return p;
+            }
+            sumLeft += nums[p];
+        }
+        return -1;
+
+    }
+}
