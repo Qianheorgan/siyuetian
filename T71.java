45fdc212b36cdcc9ee9c8b6a87b88409326b4291
Qianheorgan <2238571572@qq.com>
2020-01-26 22:39:06 +0800

    T71.java

---
+++ T71.java
@@ -0,0 +1,10 @@
+public class T71 {
+    public void loop() {
+         int x= 10;
+        while ( true ) {
+            System.out.print("x minus one is " + (x - 1));
+            x -= 1;
+        }
+    }
+}
+
