b83184d5aee143142cb720bb1da165d26d78964a
Qianheorgan <2238571572@qq.com>
2020-01-29 23:16:59 +0800

    子集

---
+++ T131.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class T131 {
+    public static List<List<Integer>> binaryBit(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        for (int i = 0; i < (1 << nums.length); i++) {
+            List<Integer> sub = new ArrayList<Integer>();
+            for (int j = 0; j < nums.length; j++)
+                if (((i >> j) & 1) == 1) sub.add(nums[j]);
+            res.add(sub);
+        }
+        return res;
+    }
+
+
+}
