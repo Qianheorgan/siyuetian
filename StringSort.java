86d6b9860d938ba6876243164d0b27abf32c789d
Qianheorgan <2238571572@qq.com>
2020-02-04 21:28:26 +0800

    字符串排序

---
+++ StringSort.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+public class StringSort{
+    /**     * 每次把后6位数字切出来丢进数组，之后排序     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            String s = bf.readLine();
+            ans[i] = Integer.parseInt(s.substring(s.length()-6,s.length()));
+        }
+        Arrays.sort(ans);
+        for (int i = 0; i < ans.length; i++) {
+            System.out.println(ans[i]);
+        }
+    }
+}
