9526e3caa3b6c1f0321d3ac2d2d9f50104b411cb
Qianheorgan <2238571572@qq.com>
2020-02-10 22:00:13 +0800

    员工考勤记录

---
+++ Checking.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+public class Checking{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int len = input.nextInt();
+        if (len == 1)
+            System.out.println(1);
+        else if (len == 2)
+            System.out.println(4);
+        else{
+            System.out.println(len*(len-1)/2+len+1);
+        }
+    }
+}
