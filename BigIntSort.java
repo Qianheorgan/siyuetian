a82a1cea7a9b57fc08940e018542c048971165e2
Qianheorgan <2238571572@qq.com>
2020-03-07 22:55:46 +0800

    大整数排序

---
+++ BigIntSort.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+import java.math.*;
+public class BigIntSort{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        while(s.hasNext()){
+            int n = s.nextInt();
+            BigInteger[] bg = new BigInteger[n];
+            for(int i=0; i<n; i++)
+                bg[i] = s.nextBigInteger();
+            Arrays.sort(bg);
+            for(int i=0; i<n; i++)
+                System.out.println(bg[i]);
+        }
+    }
+}
