621be1657380feb6617d2b97816de65f4ac12b39
Qianheorgan <2238571572@qq.com>
2020-09-04 23:20:22 +0800

    Six1.java

---
+++ Six1.java
@@ -0,0 +1,14 @@
+public class Six {
+    public static void main(String[] args) {
+         int k =3;
+         int t=10;
+         int d=2;
+         int s=0;
+
+          for(int i=1;i<=t;i+=k){
+
+              s+=  d;
+          }
+        System.out.println(s);
+    }
+}
