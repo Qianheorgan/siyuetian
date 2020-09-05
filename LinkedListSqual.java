abbefa6cf8b6fbac6fd70e3a08c95bf8d0ea965b
Qianheorgan <2238571572@qq.com>
2020-09-05 23:14:12 +0800

    链表有环

---
+++ LinkedListSqual.java
@@ -0,0 +1,16 @@
+public class  LinkedListSqual {//判断链表是否有环
+    public boolean hasCycle(ListNode head) {
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast!=null&&fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+
+            if(fast==slow){
+                return true;
+            }
+
+        }
+        return false;
+    }
+}
